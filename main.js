c84d62719347b325c3171827f288b7706095145a
GABRIEL SANTOS COUTINHO <gabriel.scoutinho4@senacsp.edu.br>
2025-02-20 20:52:21 -0300

    Atualização

--- main.js
+++ main.js
@@ -6,9 +6,52 @@
 //Importação do módulo de conexão (database.js)
 const {conectar, desconectar } = require('./database.js')
 
+// Immportação do modelo de dados de clientes
+const clienteModel = require('./src/models/Clientes.js')
+
+//CRUD Create (função para adicionar um novo cliente)
+const criarCliente = async (nomeCli, foneCli) => {
+    try {
+        const novoCliente = new clienteModel (
+            {
+            nomeCliente: nomeCli,
+            foneCliente: foneCli
+
+            }
+            
+        )
+        // A linha abaixo salva os dados do cliente no banco
+        await novoCliente.save()
+        console.log("Cliente adicionado com sucesso ")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+
+const criarCliente1 = async (nomeCli1, foneCli1) => {
+    try {
+        const novoCliente1 = new clienteModel (
+            {
+            nomeCliente: nomeCli1,
+            foneCliente: foneCli1
+
+            }
+            
+        )
+        // A linha abaixo salva os dados do cliente no banco
+        await novoCliente1.save()
+        console.log("Cliente adicionado com sucesso ")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
+
 //exexução de aplicação 
 const app = async () => {
     await conectar()
+    await criarCliente("Coutinho", "99999-0000")
     await desconectar()
 }
 
